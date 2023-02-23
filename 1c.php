2614d3a0fc9fc9f1df80c930884496251af11850
zackyazmi2800 <120625416+zackyazmi2800@users.noreply.github.com>
2023-02-23 09:35:33 +0700

    Update 1c.php

--- 1c.php
+++ 1c.php
@@ -4,93 +4,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>1c</title>
     <style>
-        html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-	margin: 0;
-	padding: 0;
-	border: 0;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
-}
-body {
-	line-height: 1;
-}
-ol, ul {
-	list-style: none;
-}
-blockquote, q {
-	quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-	content: '';
-	content: none;
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-}
-
-
-.container{
-    margin-top:50px;
-    width: 350px;
-    height: 350px;
-
-}
-div{
-    display: inline-block;
-}
-.atas{
-    width: 300px;
-    display:inline-block;
-}
-.tengah{
-    width: 300px;
-    display:inline-block;
-}
-.kotak{
-    width: 100px;
-    height: 100px;
-    background-color: red;
-    border:1px solid black;
-    text-align:center;
-    line-height:100px;
- 
-}
+        table{
+            background-color:blue;
+        }
     </style>
 </head>
 <body>
-    <div class="container">
-
-        <div class="atas">
-        <div class="kotak"><p>1</p></div>
-        </div>
-        <div class="tengah"> 
-            <div class="kotak"><p>2</p></div><div class="kotak"><p>2</p></div>
-        </div>
-        <div class="bawah"> 
-            <div class="kotak"><p>3</p></div><div class="kotak"><p>3</p></div><div class="kotak"><p>3</p></div>
-        </div>
-    </div>
+    <table border="1" cellpadding="20" cellspacing="0">
+    <tr>
+        <td>1</td>
+    </tr>
+</table>
+<table border="1" cellpadding="20" cellspacing="0">
+<tr>
+    <td>2</td>
+    <td>2</td>
+</tr>
+</table>
+<table border="1" cellpadding="20" cellspacing="0">
+<tr>
+    <td>3</td>
+    <td>3</td>
+    <td>3</td>
+</tr>
+<table>
 </body>
 </html>
